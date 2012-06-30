4c0bd662420d5c3e2b87e28e73b75d04c63472f9
David Thompson <dthompson@member.fsf.org>
2012-06-30 15:25:51 -0400

    Made a new map file with some new tilesets. Also updated .gitignore.

--- rpg-html/src/net/davexunit/rpg/client/GwtLauncher.java
+++ rpg-html/src/net/davexunit/rpg/client/GwtLauncher.java
@@ -1,6 +1,6 @@
 package net.davexunit.rpg.client;
 
-import net.davexunit.rpg.RPGOld;
+import net.davexunit.rpg.RPG;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
@@ -14,6 +14,6 @@ public class GwtLauncher extends GwtApplication {
 
 	@Override
 	public ApplicationListener getApplicationListener () {
-		return new RPGOld();
+		return new RPG();
 	}
 }
